fbe79383e7c4be20103ca7aa23db89d82a5eb927
amrbou <134005375+amrbou@users.noreply.github.com>
2025-10-28 10:22:06 +0100

    CORRECTION SERVER.PORT, set manuellement au lieu auto ajout bootstrap starter dans pom.xml

--- src/main/java/com/myHR/api_sb/ApiSbApplication.java
+++ src/main/java/com/myHR/api_sb/ApiSbApplication.java
@@ -2,8 +2,10 @@ package com.myHR.api_sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiSbApplication {
 
 	public static void main(String[] args) {
