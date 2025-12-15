91243ebfd1b330fb8f8b19a689a59b84f0750f95
amrbou <134005375+amrbou@users.noreply.github.com>
2025-12-15 05:34:18 +0100

    Ajout config microservice interface

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,39 @@
+-- ID 1
 INSERT INTO employees (first_name, last_name, mail, password)
 VALUES ('Laurent', 'GINA', 'laurentgina@mail.com','laurent');
 
+-- ID 2
 INSERT INTO employees (first_name, last_name, mail, password)
 VALUES ('Sophie', 'FONCEK', 'sophiefoncek@mail.com','sophie');
 
+-- ID 3
 INSERT INTO employees (first_name, last_name, mail, password)
 VALUES ('Agathe', 'FEELING', 'agathefeeling@mail.com','agathe');
+
+-- ID 4
+INSERT INTO employees (first_name, last_name, mail, password)
+VALUES ('Jean', 'BON', 'jean.bon@mail.com', 'jean');
+
+-- ID 5
+INSERT INTO employees (first_name, last_name, mail, password)
+VALUES ('Paul', 'OCHON', 'paul.ochon@mail.com', 'paul');
+
+-- ID 6
+INSERT INTO employees (first_name, last_name, mail, password)
+VALUES ('Marie', 'TIME', 'marie.time@mail.com', 'marie');
+
+-- ID 7
+INSERT INTO employees (first_name, last_name, mail, password)
+VALUES ('Vincent', 'TIM', 'vincent.tim@mail.com', 'vincent');
+
+-- ID 8
+INSERT INTO employees (first_name, last_name, mail, password)
+VALUES ('Alain', 'TERIEUR', 'alain.terieur@mail.com', 'alain');
+
+-- ID 9
+INSERT INTO employees (first_name, last_name, mail, password)
+VALUES ('Alex', 'TERIEUR', 'alex.terieur@mail.com', 'alex');
+
+-- ID 10
+INSERT INTO employees (first_name, last_name, mail, password)
+VALUES ('Sarah', 'CROCHE', 'sarah.croche@mail.com', 'sarah');
